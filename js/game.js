f9bd2e1b1f9953df99bba80191017844c3b35fa2
SamairaKundra <65811977+SamairaKundra@users.noreply.github.com>
2020-10-21 16:58:32 +0530

    Add files via upload

---
+++ js/game.js
@@ -0,0 +1,130 @@
+class Game{
+    constructor(){
+
+    }
+    getState() {
+        var gameStateRef = database.ref('gameState');
+        gameStateRef.on("value", function (data) {
+            gameState = data.val();
+        })
+
+    }
+
+    update(state) {
+        database.ref('/').update({
+            gameState: state
+        });
+    }
+    async start() {
+            if (gameState === 0) {
+                player = new Player();
+                var playerCountRef = await database.ref('playerCount').once("value");
+                if (playerCountRef.exists()) {
+                    playerCount = playerCountRef.val();
+                    player.getCount();
+                }
+                form = new Form()
+                form.display();
+            }
+    player1 = createSprite(200,500);
+    player1.addImage("player1",player_img);
+    p1score = 0;
+    
+    player2 = createSprite(800,500);
+    player2.addImage("player2", player_img);
+    p2score = 0;
+    players=[player1,player2];
+
+        }
+    
+    play(){
+        
+                form.hide();
+
+                Player.getPlayerInfo();
+                 image(back_img, 0, 0, 1000, 800);
+                 var x =100;
+                 var y=200;
+                 var index =0;
+                 drawSprites();
+                 for(var plr in allPlayers){
+                    
+                    
+                     index = index+1;
+                     x = 500-allPlayers[plr].distance;
+                     y=500;
+                     
+                     players[index -1].x = x;
+                     players[index - 1].y = y;
+                       
+                     if(index === player.index){
+                         
+                        fill("black");
+                         textSize(25);
+                         text(allPlayers[plr].name ,x-25,y+25);
+
+                         
+                     }
+            
+                     fill("white");
+                     textSize(35);
+                     text("Player1 Score:"+p1score,500,100);
+                     text("Player2 Score:"+p2score,500,200);
+                 }
+                
+                
+                 
+
+                if (keyIsDown(RIGHT_ARROW) && player.index !== null) {
+                    player.distance += 10
+                    player.update();
+                }
+                if (keyIsDown(LEFT_ARROW) && player.index !== null) {
+                    player.distance -= 10
+                    player.update();
+                }
+            
+                 if (frameCount % 20 === 0) {
+                     fruits = createSprite(random(100, 1000), 0, 100, 100);
+                     fruits.velocityY = 6;
+                     var rand = Math.round(random(1,5));
+                     switch(rand){
+                         case 1: fruits.addImage("fruit1",fruit1_img);
+                         break;
+                         case 2: fruits.addImage("fruit1", fruit2_img);
+                         break;
+                         case 3: fruits.addImage("fruit1", fruit3_img);
+                         break;
+                         case 4: fruits.addImage("fruit1", fruit4_img);
+                         break;
+                         case 5: fruits.addImage("fruit1", fruit5_img);
+                         break;
+                     }
+                     fruitGroup.add(fruits);
+                     
+                 }
+                 if(fruitGroup.x==player1.x & fruitGroup.y==player1.y){
+                 p1score = p1score+1;
+                 fruitGroup.destroyEach; 
+                 }
+                 if(fruitGroup.x==player2.x & fruitGroup.y==player2.y){
+                 p2score = p2score+1;
+                 fruitGroup.destroyEach;
+                 }
+
+                  if (player.index !== null) {
+                   
+                  }
+                 
+
+         
+         
+        
+         
+
+    }
+
+    end(){
+       console.log("Game Ended");
+    }
+}
